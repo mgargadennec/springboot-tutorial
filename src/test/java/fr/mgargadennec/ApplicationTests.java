ec7cefd70eda2ef61499b7f0cb4e1a3637ba2e99
Maël Gargadennec <mael.gargadennec@gmail.com>
2016-02-23 15:20:18 +0100

    Starting documentation
    Signed-off-by: Maël Gargadennec <mael.gargadennec@gmail.com>

--- src/test/java/fr/mgargadennec/ApplicationTests.java
+++ src/test/java/fr/mgargadennec/ApplicationTests.java
@@ -1,13 +1,15 @@
-package com.example;
+package fr.mgargadennec;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import fr.mgargadennec.Application;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DemoApplication.class)
-public class DemoApplicationTests {
+@SpringApplicationConfiguration(classes = Application.class)
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
