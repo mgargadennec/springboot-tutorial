716a771669390afe55d915d9b2a80e7ecab2433e
Maël Gargadennec <mael.gargadennec@gmail.com>
2016-02-24 18:31:39 +0100

    Added test of scope prototype Signed-off-by: Maël Gargadennec <mael.gargadennec@gmail.com>

--- project/src/main/java/fr/mgargadennec/Application.java
+++ project/src/main/java/fr/mgargadennec/Application.java
@@ -1,9 +1,13 @@
 package fr.mgargadennec;
 
+import java.util.UUID;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 
 import fr.mgargadennec.tools.HelloWorld;
 import fr.mgargadennec.tools.HelloWorldPrinter;
@@ -13,37 +17,43 @@ public class Application {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(Application.class);
-		
+
 		System.out.println("Listing available beans");
-		
-		for(String beanName : context.getBeanDefinitionNames()){
+
+		for (String beanName : context.getBeanDefinitionNames()) {
 			System.out.println(beanName);
 		}
-		
+
 	}
-	
+
 	@Bean
-	public String hello(){
+	public String hello() {
 		return "hello";
 	}
-	
+
 	@Bean
-	public String world(){
+	public String world() {
 		return "world";
 	}
 
 	@Bean
-	public HelloWorld helloWorld(){
+	public HelloWorld helloWorld() {
 		return new HelloWorld(hello(), world());
 	}
-	
-	@Bean(name="fooBar")
-	public HelloWorld someRandomName(){
-		return new HelloWorld("foo","bar");
+
+	@Bean(name = "fooBar")
+	public HelloWorld someRandomName() {
+		return new HelloWorld("foo", "bar");
 	}
-	
+
 	@Bean
-	public HelloWorldPrinter printer(){
+	public HelloWorldPrinter printer() {
 		return new HelloWorldPrinter(helloWorld());
+	}
+
+	@Bean
+	@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public String testScope() {
+		return UUID.randomUUID().toString();
 	}
 }
