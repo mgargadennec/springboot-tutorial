79c408b8a8d9b92a794a8d3fa2ef4d8511aa28d3
Maël Gargadennec <mael.gargadennec@gmail.com>
2016-02-24 18:07:42 +0100

    Exercice 1 correction
    Signed-off-by: Maël Gargadennec <mael.gargadennec@gmail.com>

--- project/src/main/java/fr/mgargadennec/Application.java
+++ project/src/main/java/fr/mgargadennec/Application.java
@@ -1,9 +1,49 @@
 package fr.mgargadennec;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import fr.mgargadennec.tools.HelloWorld;
+import fr.mgargadennec.tools.HelloWorldPrinter;
+
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(Application.class);
+		
+		System.out.println("Listing available beans");
+		
+		for(String beanName : context.getBeanDefinitionNames()){
+			System.out.println(beanName);
+		}
 		
 	}
 	
+	@Bean
+	public String hello(){
+		return "hello";
+	}
+	
+	@Bean
+	public String world(){
+		return "world";
+	}
+
+	@Bean
+	public HelloWorld helloWorld(){
+		return new HelloWorld(hello(), world());
+	}
+	
+	@Bean(name="fooBar")
+	public HelloWorld someRandomName(){
+		return new HelloWorld("foo","bar");
+	}
+	
+	@Bean
+	public HelloWorldPrinter printer(){
+		return new HelloWorldPrinter(helloWorld());
+	}
 }
